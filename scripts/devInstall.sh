1feeeca1b425d6142d82ec144d99a1a4ca867179
Alessandro <dixiedream@hotmail.it>
2022-10-01 18:02:14 +0200

    Now using ansible-playbook instead of ansible-pull for dev install too

--- scripts/devInstall.sh
+++ scripts/devInstall.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-branch="${1:-main}"
-ansible-pull -U https://github.com/dixiedream/ansible -K dev.yml -C $branch
+# branch="${1:-main}"
+# ansible-pull -U https://github.com/dixiedream/ansible -K dev.yml -C $branch
+ansible-playbook -K dev.yml
