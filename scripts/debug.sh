d7d54666dcb4106f2de4beb4a9f21a59d6c8f01f
Alessandro <dixiedream@hotmail.it>
2025-10-17 00:48:06 +0200

    it worked

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -2,6 +2,6 @@
 
 playbook=${1:-local.yml}
 hosts=${2:-all}
-inventory=${3:-inventory}
+inventory=${3:-hosts}
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $playbook -vv --limit $hosts -i $inventory --step
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -K $playbook -vv --limit $hosts -i $inventory --step
